3d8f7b83da87dd54033433561c5bed6d9c164b22
iocseb <seb@eml.cc>
2022-07-27 21:47:17 -0700

    removed flawed test file

--- Tests/test_argument_parser.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-from lognet import get_argument_parser
-
-
-def test_get_argument_parser():
-    """ Test for get_argument_parser """
-
-    assert type(get_argument_parser()) == argparse.Namespace
